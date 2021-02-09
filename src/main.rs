3c00e2f1752ddf88b8a4ae4aa28e06eeb1d01e5e
Rohan Gautam <rohan1gautam@gmail.com>
2021-02-09 13:25:59 +0800

    simplify base example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate lazy_static;
+use prometheus::{
+    HistogramOpts, HistogramVec, IntCounter, IntCounterVec, IntGauge, Opts, Registry,
+};
+use rand::prelude::*;
+use std::result::Result;
+use std::time::Duration;
+use warp::{Filter, Rejection, Reply};
+
+const ENVS: &'static [&'static str] = &["testing", "production"];
+
+lazy_static! {
+    pub static ref RESPONSE_TIME_COLLECTOR: HistogramVec = HistogramVec::new(
+        HistogramOpts::new("response_time", "Response Times"),
+        &["env"]
+    )
+    .expect("metric can be created");
+    pub static ref REGISTRY: Registry = Registry::new();
+}
+
+#[tokio::main]
+async fn main() {
+    register_custom_metrics();
+
+    let metrics_route = warp::path!("metrics").and_then(metrics_handler);
+
+    tokio::task::spawn(data_collector());
+
+    println!("Started on port 8080");
+    warp::serve(metrics_route).run(([0, 0, 0, 0], 8080)).await;
+}
+
+fn register_custom_metrics() {
+    REGISTRY
+        .register(Box::new(RESPONSE_TIME_COLLECTOR.clone()))
+        .expect("collector can be registered");
+}
+
+async fn data_collector() {
+    let mut collect_interval = tokio::time::interval(Duration::from_millis(10));
+    loop {
+        collect_interval.tick().await;
+        let mut rng = thread_rng();
+        let response_time: f64 = rng.gen_range(20.0, 300.0);
+        // let response_code: usize = rng.gen_range(100, 599);
+        let env_index: usize = rng.gen_range(0, 2);
+
+        track_request_time(response_time, ENVS.get(env_index).expect("exists"));
+        // track_request_time(response_time, "testing")
+    }
+}
+
+fn track_request_time(response_time: f64, env: &str) {
+    RESPONSE_TIME_COLLECTOR
+        .with_label_values(&[env])
+        .observe(response_time);
+}
+
+async fn metrics_handler() -> Result<impl Reply, Rejection> {
+    use prometheus::Encoder;
+    let encoder = prometheus::TextEncoder::new();
+
+    let mut buffer = Vec::new();
+    if let Err(e) = encoder.encode(&REGISTRY.gather(), &mut buffer) {
+        eprintln!("could not encode custom metrics: {}", e);
+    };
+    let mut res = match String::from_utf8(buffer.clone()) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("custom metrics could not be from_utf8'd: {}", e);
+            String::default()
+        }
+    };
+    buffer.clear();
+
+    let mut buffer = Vec::new();
+    if let Err(e) = encoder.encode(&prometheus::gather(), &mut buffer) {
+        eprintln!("could not encode prometheus metrics: {}", e);
+    };
+    let res_custom = match String::from_utf8(buffer.clone()) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("prometheus metrics could not be from_utf8'd: {}", e);
+            String::default()
+        }
+    };
+    buffer.clear();
+
+    res.push_str(&res_custom);
+    Ok(res)
 }
